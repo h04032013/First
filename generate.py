29b0898c51628a488c27ef82a28a46ee790e4feb
Haylin Diaz <hdiaz@g.harvard.edu>
2024-10-01 14:41:36 -0400

    adding generate

---
+++ generate.py
@@ -0,0 +1,56 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3.5-mini-instruct")
+
+# add the EOS token as PAD token to avoid warnings
+model = AutoModelForCausalLM.from_pretrained("microsoft/Phi-3.5-mini-instruct", pad_token_id=tokenizer.eos_token_id).to(device)
+
+# encode context the generation is conditioned on
+model_inputs = tokenizer('I am allergic to meat.', return_tensors='pt').to(torch_device)
+
+# generate 40 new tokens
+greedy_output = model.generate(**model_inputs, max_new_tokens=40)
+
+print("Output:\n" + 100 * '-')
+print(tokenizer.decode(greedy_output[0], skip_special_tokens=True))
+
+# activate beam search and early_stopping
+beam_output = model.generate(
+    **model_inputs,
+    max_new_tokens=40,
+    num_beams=5,
+    early_stopping=True
+)
+
+print("Output:\n" + 100 * '-')
+print(tokenizer.decode(beam_output[0], skip_special_tokens=True))
+
+# set no_repeat_ngram_size to 2
+beam_output = model.generate(
+    **model_inputs,
+    max_new_tokens=40,
+    num_beams=5,
+    no_repeat_ngram_size=2,
+    early_stopping=True
+)
+
+print("Output:\n" + 100 * '-')
+print(tokenizer.decode(beam_output[0], skip_special_tokens=True))
+
+# set seed to reproduce results. Feel free to change the seed though to get different results
+from transformers import set_seed
+set_seed(42)
+
+# activate sampling and deactivate top_k by setting top_k sampling to 0
+sample_output = model.generate(
+    **model_inputs,
+    max_new_tokens=40,
+    do_sample=True,
+    top_k=0
+)
+
+print("Output:\n" + 100 * '-')
+print(tokenizer.decode(sample_output[0], skip_special_tokens=True))
