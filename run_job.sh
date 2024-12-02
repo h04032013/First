3dea860e1a8c0df6e273c7b83173453e5e9537f6
h04032013 <88753465+h04032013@users.noreply.github.com>
2024-12-02 11:48:46 -0500

    Update run_job.sh
    From web, failed to install bfg  for large file history
    -conda is gone

--- run_job.sh
+++ run_job.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-#SBATCH --job-name=my_job
+#SBATCH --job-name=h_ftjob
 #SBATCH --account=kempner_dam_lab
 #SBATCH --partition=kempner
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
-#SBATCH --gpus-per-node=1
-#SBATCH --time=0-01:00
-#SBATCH --mem=32G
+#SBATCH --gpus-per-node=4
+#SBATCH --time=0-05:00:00
+#SBATCH --mem=128G
 #SBATCH --output=my_job_output.out
 #SBATCH --error=my_job_error.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=hdiaz@g.harvard.edu
+cd /n/home00/hdiaz/StarterProj
+
 
 # Load modules
 module load python/3.10.9-fasrc01 
 
 # Activate conda environment (optional)
+mamba activate myenv
 
 # Run the job
-python generate.py
+python Finetune.py
